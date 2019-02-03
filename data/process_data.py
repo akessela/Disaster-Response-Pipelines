fae9d1f10715557413163834b28a6c099f485d6d
Alazar <alazar.kessela@gmail.com>
2019-02-02 21:35:01 -0500

    updated process_data.py with all functions implemented

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,51 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    read messages and categories files into a pandas dataframe.
+    Merge the two data frames.
+    :param messages_filepath:
+    :param categories_filepath:
+    :return: df  pandas dataframe
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id', how='inner')
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    clean dataframe and return it.
+    :param df: rpandas dataframe
+    :return: df pandas dataframe
+
+    """
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[1, :]
+    category_names = row.apply(lambda x: x[:-2])
+    categories.columns = category_names
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: int(x[-1]))
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    save pandas dataframe to sqlite Database.
+    :param df:
+    :param database_filename:
+    :return: None
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('Response', engine, index=False)
 
 
 def main():
